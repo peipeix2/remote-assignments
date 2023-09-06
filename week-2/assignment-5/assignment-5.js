be378806467d35ff1a2bff55da0783359a82425b
peipeix2 <peipei.hsueh@outlook.com>
2023-09-06 12:29:49 +0800

    Add week-2 assignment folders

---
+++ week-2/assignment-5/assignment-5.js
@@ -0,0 +1,11 @@
+function twoSum(nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    if (nums.includes(target - nums[i])) {
+      let secondIndex = nums.indexOf(target - nums[i])
+      return [i, secondIndex]
+    }
+  }
+  return `Can't find any solution.`
+} 
+
+console.log(twoSum([2, 7, 11, 15], 9))
