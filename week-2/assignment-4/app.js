97a9d02fd4d3925c3cc2334d44d283b06ff26678
peipeix2 <peipei.hsueh@outlook.com>
2023-09-06 13:10:11 +0800

    Add functions to hamburger button to show and close dropdown menu when width < 800px

--- week-2/assignment-4/app.js
+++ week-2/assignment-4/app.js
@@ -1,6 +1,9 @@
 const welcomeBlock = document.querySelector('.landing-page-img')
 const readMoreButton = document.querySelector('.landing-page-button')
 const hiddenContainer = document.querySelector('.hide')
+const hamburgerBar = document.querySelector('.hamburger-bar')
+const dropdownMenu = document.querySelector('.hamburger-items')
+const closeIcon = document.querySelector('.close-icon')
 
 // Click the welcome block to change text
 welcomeBlock.addEventListener('click', (event) => {
@@ -18,4 +21,14 @@ readMoreButton.addEventListener('click', (event) => {
     event.target.removeAttribute('style')
     event.target.textContent = 'READ MORE'
   }
+})
+
+// Click the hamburger icon to show menu
+hamburgerBar.addEventListener('click', (event) => {
+  dropdownMenu.style.right = '0px'
+})
+
+// Click the close icon on the menu to close it
+closeIcon.addEventListener('click', (event) => {
+  dropdownMenu.style.right = '-200px'
 })
