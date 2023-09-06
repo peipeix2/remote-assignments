5bee1ae64257f6e14f17d55a129b860b0cb2e616
peipeix2 <peipei.hsueh@outlook.com>
2023-09-06 13:00:24 +0800

    Add click event to read-more button so that users can show and hide contents

--- week-2/assignment-4/app.js
+++ week-2/assignment-4/app.js
@@ -1,6 +1,21 @@
 const welcomeBlock = document.querySelector('.landing-page-img')
+const readMoreButton = document.querySelector('.landing-page-button')
+const hiddenContainer = document.querySelector('.hide')
 
 // Click the welcome block to change text
 welcomeBlock.addEventListener('click', (event) => {
   event.target.firstElementChild.textContent = 'Have a Good Time!'
+})
+
+// Click the call-to-action button to show more cards
+readMoreButton.addEventListener('click', (event) => {
+  if (!hiddenContainer.style.display) {
+    hiddenContainer.style.display = 'flex'
+    event.target.textContent = 'HIDE CONTENT'
+    event.target.style.backgroundColor = '#666666'
+  } else {
+    hiddenContainer.removeAttribute('style')
+    event.target.removeAttribute('style')
+    event.target.textContent = 'READ MORE'
+  }
 })
