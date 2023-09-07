a82f5328109ab3925ce6bca84416a7384098b9e2
peipeix2 <peipei.hsueh@outlook.com>
2023-09-07 11:55:31 +0800

    Fix assginment-5 solution: add another hypothesis where target shouldn't be twice the same number

--- week-2/assignment-5.js
+++ week-2/assignment-5.js
@@ -1,7 +1,7 @@
 function twoSum(nums, target) {
   for (let i = 0; i < nums.length; i++) {
-    if (nums.includes(target - nums[i])) {
-      let secondIndex = nums.indexOf(target - nums[i])
+    if (nums.includes(target - nums[i]) && (target !== 2 * nums[i])) {
+      const secondIndex = nums.indexOf(target - nums[i])
       return [i, secondIndex]
     }
   }
@@ -9,3 +9,4 @@ function twoSum(nums, target) {
 }
 
 console.log(twoSum([2, 7, 11, 15], 9))
+console.log(twoSum([2, 11, 7, 15], 22))
