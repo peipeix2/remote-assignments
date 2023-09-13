eb0af812cefab19109fb0391eea7dd1c24590110
peipeix2 <peipei.hsueh@outlook.com>
2023-09-13 19:50:49 +0800

    Create backend API that returns accumulation sum of a given number

--- week-3/app.js
+++ week-3/app.js
@@ -5,6 +5,21 @@ app.get('/', (req, res) => {
   res.send('<h1>Hello World. This is the solution to Week-3 Assignment 1.</h1>')
 })
 
+app.get('/getData', (req, res) => {
+  const number = Number(req.query.number)
+  if (!number) {
+    return res.send('Lack of Parameter.')
+  } else if (!Number.isInteger(number)) {
+    return res.send('Wrong Parameter.')
+  }
+
+  let sum = 0
+  for (let i = 1; i <= number; i++) {
+    sum += i
+  }
+  res.send(`<h1>${sum}</h1>`)
+})
+
 app.listen(3000, () => {
   console.log('The application is running on localhost:3000')
 })
