a19088fc42448e53e63ed93a7c1ad94f97904d09
peipeix2 <peipei.hsueh@outlook.com>
2023-09-19 20:04:10 +0800

    Fetch data from API and render to the webpage

---
+++ week-4/assignment-2/app.js
@@ -0,0 +1,29 @@
+const url = "https://remote-assignment.s3.ap-northeast-1.amazonaws.com/products";
+
+async function ajax(url) {
+  const response = await fetch(url)
+  const data = await response.json()
+  return data
+}
+
+function render(data) {
+  const dataPanel = document.querySelector('.row')
+  let contentHTML = ''
+  data.forEach(item => {
+    contentHTML += `
+      <div class="col">
+        <div class="card-container supervisor">
+          <div class="inner-card">
+            <h3 class="card-title">${item.name}</h3>
+            <span class='price'>${item.price}</span>
+            <p>${item.description}</p>
+          </div>
+        </div>
+      </div>
+    `
+  })
+  dataPanel.innerHTML = contentHTML
+}
+
+ajax(url)
+  .then(data => render(data))
