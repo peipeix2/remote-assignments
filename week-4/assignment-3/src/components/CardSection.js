db053413b53cec6159042a44d1b21fbb772bd412
peipeix2 <peipei.hsueh@outlook.com>
2023-09-20 21:35:39 +0800

    Use map to iterate over repetitive card content

--- week-4/assignment-3/src/components/CardSection.js
+++ week-4/assignment-3/src/components/CardSection.js
@@ -1,28 +1,37 @@
 import React from 'react'
 
+const cardContent = [
+  {
+    name: 'Animation',
+    content: 'Praesent arcu ipsum, sagittis luctus diam sed, tempus lacinia leo. Integer posuere nunc nec nisi elementum consectetur non non ante.'
+  },
+  {
+    name: 'Design',
+    content: 'Praesent arcu ipsum, sagittis luctus diam sed, tempus lacinia leo. Integer posuere nunc nec nisi elementum consectetur non non ante.'
+  },
+  {
+    name: 'Photography',
+    content: 'Praesent arcu ipsum, sagittis luctus diam sed, tempus lacinia leo. Integer posuere nunc nec nisi elementum consectetur non non ante.'
+  },
+  {
+    name: 'Business',
+    content: 'Praesent arcu ipsum, sagittis luctus diam sed, tempus lacinia leo. Integer posuere nunc nec nisi elementum consectetur non non ante.'
+  }
+]
+
 const CardSection = () => {
   return (
     <section>
       <h2 className="sub-heading">Our Service</h2>
       <div className="card-container">
-        <div className="card animation">
-          <p>Animation</p>
-          <span>Praesent arcu ipsum, sagittis luctus diam sed, tempus lacinia leo. Integer posuere nunc nec nisi elementum consectetur
-            non non ante.</span>
+        {cardContent.map(card => {
+          return (
+          <div className="card">
+          <p>{card.name}</p>
+          <span>{card.content}</span>
         </div>
-        <div className="card design">
-          <p>Design</p>
-          <span>Praesent arcu ipsum, sagittis luctus diam sed, tempus lacinia leo. Integer posuere nunc nec nisi elementum consectetur
-            non non ante.</span>
-        </div>
-        <div className="card photography">
-          <p>Photography</p>
-          <span>Praesent arcu ipsum, sagittis luctus diam sed, tempus lacinia leo. Integer posuere nunc nec nisi elementum consectetur non non ante.</span>
-        </div>
-        <div className="card business">
-          <p>Business</p>
-          <span>Praesent arcu ipsum, sagittis luctus diam sed, tempus lacinia leo. Integer posuere nunc nec nisi elementum consectetur non non ante.</span>
-        </div>
+        )
+        })}
       </div>
       <button className="landing-page-button">
         READ MORE
