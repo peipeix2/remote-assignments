049af93bd689c1ac13bc583d5951c566da0c5ac9
peipeix2 <peipei.hsueh@outlook.com>
2023-09-20 21:07:02 +0800

    Add landing image component

---
+++ week-4/assignment-3/src/components/LandingImage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LandingImage = () => {
+  return (
+    <div className="landing-page-img">
+      <h1 className="welcome-message">Explore Our World</h1>
+    </div>
+  )
+}
+
+export default LandingImage
