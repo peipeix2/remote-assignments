f3bbdbce53ff74a164f8789650c71b59903f6ece
peipeix2 <peipei.hsueh@outlook.com>
2023-09-20 21:07:28 +0800

    Import landing image component

--- week-4/assignment-3/src/App.js
+++ week-4/assignment-3/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from './components/Header'
+import LandingImage from './components/LandingImage';
 
 function App() {
   return (
     <>
       <Header />
+      <LandingImage />
       {/* <LandingImage />
       <Card /> */}
     </>
