ff05a7307c12d1944dc452150371e6da66cf244c
peipeix2 <peipei.hsueh@outlook.com>
2023-09-20 21:52:09 +0800

    Import nav-items component

--- week-4/assignment-3/src/components/Header.js
+++ week-4/assignment-3/src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import NavItems from './NavItems'
 
 const Header = () => {
   return (
@@ -8,18 +9,7 @@ const Header = () => {
           <p className="web-title">Wr.</p>
           <i className="web-logo fa-solid fa-pen-nib"></i>
         </div>
-        <div className="nav-items">
-          <div className="hamburger-items">
-            <i className="close-icon fa-solid fa-xmark"></i>
-            <ul>
-              <li><a href="https://www.google.com">About Us</a></li>
-              <li><a href="https://www.google.com">Our Works</a></li>
-              <li><a href="https://www.google.com">Our Partners</a></li>
-              <li><a href="https://www.google.com">Contact Us</a></li>
-            </ul>
-          </div>
-          <i className="hamburger-bar fa-solid fa-bars"></i>
-        </div>
+        <NavItems />
       </nav>
     </header>
   )
