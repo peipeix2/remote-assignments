0b8af4caa18a473b7635f238a0e9aa08d53fd516
peipeix2 <peipei.hsueh@outlook.com>
2023-09-20 21:24:25 +0800

    Import card section component

--- week-4/assignment-3/src/App.js
+++ week-4/assignment-3/src/App.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import Header from './components/Header'
 import LandingImage from './components/LandingImage';
+import CardSection from './components/CardSection';
 
 function App() {
   return (
     <>
       <Header />
       <LandingImage />
-      {/* <LandingImage />
-      <Card /> */}
+      <CardSection />
     </>
   );
 }
