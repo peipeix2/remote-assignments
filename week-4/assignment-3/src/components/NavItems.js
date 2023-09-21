8d4bbd9e9780ac14020a71469c359447d49cafb2
peipeix2 <peipei.hsueh@outlook.com>
2023-09-21 10:43:45 +0800

    Use map to iterate over nav-links

--- week-4/assignment-3/src/components/NavItems.js
+++ week-4/assignment-3/src/components/NavItems.js
@@ -1,5 +1,24 @@
 import React, { useEffect, useState } from 'react'
 
+const linkData = [
+  {
+    name: 'About Us',
+    link: 'https://www.google.com'
+  },
+  {
+    name: 'Our Works',
+    link: 'https://www.google.com'
+  },
+  {
+    name: 'Our Partners',
+    link: 'https://www.google.com'
+  },
+  {
+    name: 'Contact Us',
+    link: 'https://www.google.com'
+  }
+]
+
 const NavItems = () => {
   // if true, show dropdown
   const [dropDown, setDropDown] = useState(false)
@@ -27,10 +46,11 @@ const NavItems = () => {
       <div className={dropDown ? 'hamburger-items open' : 'hamburger-items'}>
         <i className="close-icon fa-solid fa-xmark" onClick={handleCloseDropdown}></i>
         <ul>
-          <li><a href="https://www.google.com">About Us</a></li>
-          <li><a href="https://www.google.com">Our Works</a></li>
-          <li><a href="https://www.google.com">Our Partners</a></li>
-          <li><a href="https://www.google.com">Contact Us</a></li>
+          {linkData.map(data => {
+            return (
+              <li><a href={data.link}>{data.name}</a></li>
+            )
+          })}
         </ul>
       </div>
       <i className="hamburger-bar fa-solid fa-bars" onClick={handleDropdown}></i>
