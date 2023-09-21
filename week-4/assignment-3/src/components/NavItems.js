407a05526d43dc748686c8dc95f60c5884a60c4b
peipeix2 <peipei.hsueh@outlook.com>
2023-09-21 10:23:35 +0800

    Add dropdown menu hook to achieve responsive dropdown menu

--- week-4/assignment-3/src/components/NavItems.js
+++ week-4/assignment-3/src/components/NavItems.js
@@ -1,10 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const NavItems = () => {
+  // if true, show dropdown
+  const [dropDown, setDropDown] = useState(false)
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth >= 800) {
+        setDropDown(false)
+      } 
+    }
+    window.addEventListener('resize', handleResize)
+  })
+
+  const handleDropdown = () => {
+      setDropDown(true)
+  }
+
+  const handleCloseDropdown = () => {
+    setDropDown(false)
+  }
+  
+
   return (
     <div className="nav-items">
-      <div className="hamburger-items">
-        <i className="close-icon fa-solid fa-xmark"></i>
+      <div className={dropDown ? 'hamburger-items open' : 'hamburger-items'}>
+        <i className="close-icon fa-solid fa-xmark" onClick={handleCloseDropdown}></i>
         <ul>
           <li><a href="https://www.google.com">About Us</a></li>
           <li><a href="https://www.google.com">Our Works</a></li>
@@ -12,7 +33,7 @@ const NavItems = () => {
           <li><a href="https://www.google.com">Contact Us</a></li>
         </ul>
       </div>
-      <i className="hamburger-bar fa-solid fa-bars"></i>
+      <i className="hamburger-bar fa-solid fa-bars" onClick={handleDropdown}></i>
     </div>
   )
 }
