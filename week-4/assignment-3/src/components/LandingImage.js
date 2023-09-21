cc29833994264bf0088a48d68aa9f38069f5c442
peipeix2 <peipei.hsueh@outlook.com>
2023-09-21 10:56:45 +0800

    Add hook to landing image and welcome message to respond to onclick event

--- week-4/assignment-3/src/components/LandingImage.js
+++ week-4/assignment-3/src/components/LandingImage.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const LandingImage = () => {
+  const [changeWord, setChangeWord] = useState(true)
+
+  const handleChangeWord = () => {
+    setChangeWord(false)
+  }
+
   return (
-    <div className="landing-page-img">
-      <h1 className="welcome-message">Explore Our World</h1>
+    <div className="landing-page-img" onClick={handleChangeWord}>
+      <h1 className="welcome-message" onClick={handleChangeWord}>{changeWord ? 'Explore Our World' : 'Have a Good Time!'}</h1>
     </div>
   )
 }
