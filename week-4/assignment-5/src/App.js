955e82af01b39448ea69cebaa05e14f3e936c538
peipeix2 <peipei.hsueh@outlook.com>
2023-09-24 11:17:39 +0800

    Render Counter

--- week-4/assignment-5/src/App.js
+++ week-4/assignment-5/src/App.js
@@ -1,24 +1,30 @@
 import logo from './logo.svg';
+import Counter from './components/Counter';
 import './App.css';
 
+const counterData = [
+  {
+    id: 1,
+    number: 0
+  },
+  {
+    id: 2,
+    number: 0
+  },
+  {
+    id: 3,
+    number: 0
+  }
+]
+
 function App() {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <Counter />
     </div>
+    
   );
 }
 
