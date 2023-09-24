21ed3f95252ae3c4671e4a6f4d3569479e615f10
peipeix2 <peipei.hsueh@outlook.com>
2023-09-24 11:38:52 +0800

    Add props to Counter and use map iteration to render

--- week-4/assignment-5/src/App.js
+++ week-4/assignment-5/src/App.js
@@ -2,27 +2,33 @@ import logo from './logo.svg';
 import Counter from './components/Counter';
 import './App.css';
 
-const counterData = [
-  {
-    id: 1,
-    number: 0
-  },
-  {
-    id: 2,
-    number: 0
-  },
-  {
-    id: 3,
-    number: 0
-  }
-]
+
 
 function App() {
-
+  const counterData = [
+    {
+      id: 1,
+      number: 0
+    },
+    {
+      id: 2,
+      number: 0
+    },
+    {
+      id: 3,
+      number: 0
+    }
+  ]
 
   return (
     <div className='container'>
-      <Counter />
+      {/* render counters */}
+      {counterData.map(counter => 
+        <Counter 
+          id={counter.id} 
+          number={counter.number} />
+      )}
+      
     </div>
     
   );
