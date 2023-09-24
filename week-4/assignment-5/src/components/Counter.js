f0851b8ec11f2ba8397a145a65160d0567dcad6c
peipeix2 <peipei.hsueh@outlook.com>
2023-09-24 13:32:05 +0800

    Add onClick event to increment button

--- week-4/assignment-5/src/components/Counter.js
+++ week-4/assignment-5/src/components/Counter.js
@@ -1,9 +1,13 @@
 import React from 'react'
 
 const Counter = (props) => {
+  const handleIncrement = () => {
+    props.changeNumber(props.id)
+  }
+
   return (
     <div className='counter'>
-      <button className='increment'>+1</button>
+      <button className='increment' onClick={handleIncrement}>+1</button>
       <p className='cur-number'>{props.number}</p>
     </div>
   )
