b219ce035fef417a93f067df8a6ca0941704c518
peipeix2 <peipei.hsueh@outlook.com>
2023-09-24 11:16:45 +0800

    add counter component

---
+++ week-4/assignment-5/src/components/Counter.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Counter = () => {
+  return (
+    <div className='counter'>
+      <button className='increment'>+1</button>
+      <p className='cur-number'>0</p>
+    </div>
+  )
+}
+
+export default Counter
